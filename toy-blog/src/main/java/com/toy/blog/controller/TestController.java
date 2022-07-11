9ce35619ab3cb52841c55461ddced74c115b030f
한민주 <alswn9986@gmail.com>
2022-07-11 15:44:05 +0900

    initial project.

---
+++ toy-blog/src/main/java/com/toy/blog/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.toy.blog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@RequestMapping("/")
+    public String hello() {
+        return "Hello Siwon!";
+    }
+}
