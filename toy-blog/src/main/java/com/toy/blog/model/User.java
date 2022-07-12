b470432e53532002ef86740e8f69d11311f3248b
alswn9986@gmail.com <alswn9986@gmail.com>
2022-07-12 11:03:29 +0900

    lombok 라이브러리 추가 User 생성자, getter/setter 어노테이션으로 변경

--- toy-blog/src/main/java/com/toy/blog/model/User.java
+++ toy-blog/src/main/java/com/toy/blog/model/User.java
@@ -1,27 +1,11 @@
 package com.toy.blog.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class User {
-	// 임시 생성자
-	public User(String name, int age) {
-		this.name = name;
-		this.age = age;
-	}
-	
-	// 필드
     private String name;
     private int age;
-    
-    // 임시 getter/setter
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
 }
